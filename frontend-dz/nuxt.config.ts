d5d693249c050191c589effdd98962ab065d6566
Zachary Taylor <zachary.taylor@L121-202106-01.lan>
2023-04-06 23:45:38 -0400

    working

--- frontend-dz/nuxt.config.ts
+++ frontend-dz/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-coznfig
 export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass', '~/assets/styles/main.scss', '@fortawesome/fontawesome-svg-core/styles.css'],
+    css: ['vuetify/lib/styles/main.sass', '~/assets/styles/main.scss'],
     build: {
       transpile: ['vuetify'],
     },
