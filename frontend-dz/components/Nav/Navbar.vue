70efb672e8fea9064bc6f9a8b3d1b9287a3a6f93
Zachary Taylor <zachary.taylor@L121-202106-01.local>
2024-02-06 17:49:50 -0500

    Rework nav

--- frontend-dz/components/Nav/Navbar.vue
+++ frontend-dz/components/Nav/Navbar.vue
@@ -1,68 +1,115 @@
 <template>
-    <nav class="navbar dz-header" role="navigation" aria-label="main navigation">
-        <div class="container">
-            <div class="navbar-brand dz-header__logo">
-                <a class="dz-header__logo-link" href="/">
-                    <img class="dz-header__logo" src="~/assets/img/digitalzac-logo.png" width="112" height="28">
-                </a>
-                <a @click="toggleMenu" role="button" class="navbar-burger" :class="menuIsActive ? 'is-active' :  '' " aria-label="menu" aria-expanded="false">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
-            <div class="navbar-menu" :class="menuIsActive ? 'is-active' :  '' ">
-                <div class="navbar-start">
-                    <a class="navbar-item" v-for="item in menuData.items.data" :key="item" :href="item.attributes.url">
-                        {{item.attributes.title}}
-                    </a>
-                </div>
-            </div>
-        </div>
-    </nav>
-</template>
+  <nav class="navbar dz-header" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="dz-header__logo-link" href="/">
+          <img
+            class="dz-header__logo"
+            src="~/assets/img/digitalzac-logo2.svg" />
+        </a>
+        <a
+          @click="toggleMenu"
+          role="button"
+          class="navbar-burger dz-header__burger"
+          :class="menuIsActive ? 'is-active' : ''"
+          aria-label="menu"
+          aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" :class="menuIsActive ? 'is-active' : ''">
+        <div class="navbar-end dz-header__links">
+          <a
+            class="navbar-item"
+            v-for="item in menuData.items.data"
+            :key="item"
+            :href="item.attributes.url">
+            {{ item.attributes.title }}
+          </a>
 
+          <UtilityButton :buttonProps="button" :classes="'dz-header__button'" />
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
 <script>
 export default {
-    name: "Navbar",
-    props: {
-        menuData: {
-            type: Object,
-            default: []
-        }
+  name: "Navbar",
+  props: {
+    menuData: {
+      type: Object,
+      default: [],
     },
-    data(){
-        return{
-            menuIsActive: false
-        }
+  },
+  data() {
+    return {
+      menuIsActive: false,
+      button: {
+        id: 6,
+        buttonText: "Get @ Me",
+        buttonLink: "#",
+        Icon: "",
+        buttonSize: "medium",
+        buttonColor: "link",
+        isOutlined: true,
+      },
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuIsActive = !this.menuIsActive;
     },
-    methods: {
-        toggleMenu(){
-            this.menuIsActive = !this.menuIsActive
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped lang="scss">
+.dz-header {
+  margin-top: 1rem;
+  &__logo {
+    max-width: 300px;
+    max-height: auto;
+  }
 
-    .dz-header{
-        &__logo{
-            width: 175px;
-            max-height: auto;
-        }
-        &__logo-link{
-            padding: 0.5rem 0.75rem;
-            display: flex;
-            align-items: center;
-        }
-        .navbar-item{
-            font-size: 20px;
-            font-weight: 500;
-            &:hover{
-                background-color: transparent;
-            }
-        }
+  &__burger {
+    span {
+      background-color: $primary;
+      border-radius: 5px;
+      height: 3px;
+      width: 20px;
     }
+  }
 
+  &__links {
+    align-items: center;
+  }
+
+  &__burger,
+  &__burger:hover {
+    background-color: transparent;
+  }
+  &__logo-link {
+    padding: 0.5rem 0.75rem;
+    display: flex;
+    align-items: center;
+  }
+  .navbar-item {
+    font-size: 20px;
+    text-transform: uppercase;
+    font-weight: bold;
+
+    &:hover {
+      background-color: transparent;
+    }
+  }
+
+  &__button {
+    margin-left: 0.75rem;
+    font-size: 20px;
+    text-transform: uppercase;
+  }
+}
 </style>
--- frontend-dz/components/Utility/Button.vue
+++ frontend-dz/components/Utility/Button.vue
@@ -1,44 +1,47 @@
 <template>
   <button
     window.location.href="https://google.com"
-    class="dz-button button" 
+    class="dz-button button"
     :class="[
-    {
+      {
         'is-outlined': buttonProps.isOutlined,
-    },
-    buttonProps.buttonColor ? 'is-' + buttonProps.buttonColor : '',
-    buttonProps.buttonSize ? 'is-' + buttonProps.buttonSize : ''
-    ]" 
-    >
+      },
+      buttonProps.buttonColor ? 'is-' + buttonProps.buttonColor : '',
+      buttonProps.buttonSize ? 'is-' + buttonProps.buttonSize : '',
+      classes,
+    ]">
     <a class="dz-button__link" :href="buttonProps.link">
-        <v-icon
+      <v-icon
         v-if="buttonProps.Icon"
         class="dz-button__icon"
-        :icon="'mdi-' + buttonProps.Icon" 
-        size="24px"/>
-        {{buttonProps.buttonText}}
+        :icon="'mdi-' + buttonProps.Icon"
+        size="24px" />
+      {{ buttonProps.buttonText }}
     </a>
   </button>
 </template>
 
 <script>
 export default {
-    name: 'UtilityButton',
-    props:{
-        buttonProps: {
-            type: Object
-        }
-    }
-}
+  name: "UtilityButton",
+  props: {
+    buttonProps: {
+      type: Object,
+    },
+    classes: {
+      type: String,
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-.dz-button{
-    &__icon{
-        margin-right: 5px;
-    }
-    &__link{
-        color: inherit !important;
-    }
+.dz-button {
+  &__icon {
+    margin-right: 5px;
+  }
+  &__link {
+    color: inherit !important;
+  }
 }
 </style>
